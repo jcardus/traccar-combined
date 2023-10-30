58e3b26fd7696b8584a164948275675f2b79030e
jcardus <asklocation.net@gmail.com>
2023-10-30 01:38:23 +0000

    check deviceid

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const axios = require('axios').create({
     withCredentials: true
 });
 
-functions.http('combined', async (req, res) => {
+functions.http('helloHttp', async (req, res) => {
     axios.defaults.headers.common['cookie'] = req.header('cookie')
-    const positions = await axios.get(req.originalUrl.replace('combined', 'route')).then(d => d.data)
-    const events = await axios.get(req.originalUrl.replace('combined', 'events')).then(d => d.data)
-    if (Array.isArray(req.query.deviceId)) {
-        req.query.deviceId.forEach(d => {})
+    if (req.originalUrl.includes('deviceId')) {
+        const positions = await axios.get(req.originalUrl.replace('combined', 'route')).then(d => d.data)
+        const events = await axios.get(req.originalUrl.replace('combined', 'events')).then(d => d.data)
+        if (Array.isArray(req.query.deviceId)) {
+            req.query.deviceId.forEach(d => {
+            })
+        }
+        res.send({positions, events});
     }
-    res.send({positions, events});
 });
